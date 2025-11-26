372cc11bfcceedf78ef7b422ad3a6b6a9621ba27
iqnuxul <mluxunqi@gmail.com>
2025-11-26 23:01:29 +0100

    Fix SSR error by disabling prerendering for WhichwitchApp

--- src/ui/app/page.tsx
+++ src/ui/app/page.tsx
@@ -1,4 +1,11 @@
-import { WhichwitchApp } from "@/components/whichwitch/app-container"
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const WhichwitchApp = dynamic(
+  () => import('@/components/whichwitch/app-container').then(mod => ({ default: mod.WhichwitchApp })),
+  { ssr: false }
+)
 
 export default function Page() {
   return <WhichwitchApp />
